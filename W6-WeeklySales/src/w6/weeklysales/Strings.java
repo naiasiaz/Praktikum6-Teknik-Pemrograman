4e618c2e8648a261f9e39e49f4a63c59df2bd4e5
naiasiaz <naiasitiazzahra27@gmail.com>
2023-02-26 22:27:36 +0700

    Studi Kasus 3

---
+++ W6-WeeklySales/src/w6/weeklysales/Strings.java
@@ -0,0 +1,38 @@
+package w6.weeklysales;
+
+/**
+ * @author Naia Siti Az-zahra
+ * @version 1.0
+ * @since 2023-02-23
+ * @description Demonstrates selectionSort on an array of integers
+ */
+
+import java.util.Scanner;
+
+public class Strings {
+    // Reads in an array of Strings, sorts them, then prints them in sorted order
+    public static void main(String[] args) {
+        String[] strList;
+        int size;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("\nHow many strings do you want to sort? ");
+        size = scan.nextInt();
+        strList = new String[size];
+        
+        System.out.println("\nEnter the strings...");
+        for(int i = 0; i < size; i++)
+            strList[i] = scan.next();
+        
+        Sorting.insertionSort(strList);
+        
+        System.out.println("\nYour strings in sorted order...");
+        for(int i = 0; i < size; i++) {
+            System.out.println(strList[i] + " ");
+        }
+        System.out.println();
+        
+        scan.close();
+    }
+}
